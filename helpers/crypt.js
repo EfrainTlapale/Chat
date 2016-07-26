cb1ce394d14f95292bc9cdd0a365fa33243b56d5
Efrain Tlapale <tlapalepulidoefrain@gmail.com>
2016-07-26 16:59:02 -0500

    Crypt configuration

--- helpers/crypt.js
+++ helpers/crypt.js
@@ -1,14 +1,14 @@
 const crypto = require('crypto');
 
 exports.encrypt = function(text){
-  var cipher = crypto.createCipher('aes-256-cbc','cbduicbduisbuewwef568485""$+2634');
+  var cipher = crypto.createCipher('aes-256-cbc',process.env.CRYPT || 'cbduicbduisbuewwef568485""$+2634');
   var crypted = cipher.update(text.toString(),'utf8','hex')
   crypted += cipher.final('hex');
   return crypted;
 }
 
 exports.decrypt = function(text){
-  var decipher = crypto.createDecipher('aes-256-cbc','cbduicbduisbuewwef568485""$+2634');
+  var decipher = crypto.createDecipher('aes-256-cbc',process.env.CRYPT || 'cbduicbduisbuewwef568485""$+2634');
   var dec = decipher.update(text.toString(),'hex','utf8')
   dec += decipher.final('utf8');
   return dec;
